737005ada37b966904ea10211de9a8cda3fdbfaa
Jazmyn <jazmyn@umich.edu>
2019-03-23 23:08:49 +0000

    edited models

--- models.py
+++ models.py
@@ -4,9 +4,15 @@ from django.db import models
 
 class User(models.Model) :
     name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    username = models.CharField(max_length=128)
 
     def __str__(self) :
         return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    date_created = models.CharField(max_length=128)
 
 class Store(models.Model) :
     name = models.CharField(max_length=128)
@@ -20,10 +26,10 @@ class Date(models.Model) :
     def __str__(self) :
         return self.name
 
-class Group(models.Model):
-    name = models.CharField(max_length=128)
-    cost = models.DecimalField(max_digits=19,decimal_places=10)
+class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    cost = models.DecimalField(max_digits=19,decimal_places=10)
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
     date = models.ForeignKey(Date, on_delete=models.CASCADE)
 
